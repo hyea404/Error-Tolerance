a29f1a436e5214a089f413e2a6ef59b4547c071c
Rokhman <128242487+hyea404@users.noreply.github.com>
2023-12-12 19:53:20 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Error_Tolerance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonBrowseFile = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Folder = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DimGray;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(186, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(602, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Path";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonBrowseFile
+            // 
+            this.buttonBrowseFile.Location = new System.Drawing.Point(12, 166);
+            this.buttonBrowseFile.Name = "buttonBrowseFile";
+            this.buttonBrowseFile.Size = new System.Drawing.Size(81, 26);
+            this.buttonBrowseFile.TabIndex = 2;
+            this.buttonBrowseFile.Text = "Open File";
+            this.buttonBrowseFile.UseVisualStyleBackColor = true;
+            this.buttonBrowseFile.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DimGray;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("OCR-A BT", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.Location = new System.Drawing.Point(258, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(312, 36);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Error Tolerance";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AllowDrop = true;
+            this.textBox3.BackColor = System.Drawing.Color.DimGray;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox3.Location = new System.Drawing.Point(476, 78);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(312, 319);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AllowDrop = true;
+            this.textBox4.BackColor = System.Drawing.Color.DimGray;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox4.Location = new System.Drawing.Point(476, 403);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(312, 35);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Mohamad Aenur Rokhman\r\nRizal Darusman\r\n";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Folder
+            // 
+            this.Folder.Location = new System.Drawing.Point(99, 166);
+            this.Folder.Name = "Folder";
+            this.Folder.Size = new System.Drawing.Size(81, 26);
+            this.Folder.TabIndex = 7;
+            this.Folder.Text = "Open Folder";
+            this.Folder.UseVisualStyleBackColor = true;
+            this.Folder.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.DimGray;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(186, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 316);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "C++";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "C#";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(58, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Python";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(48, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Java";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Location = new System.Drawing.Point(12, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 114);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(707, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 26);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Help?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Folder);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonBrowseFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.Text = "Error Tolerance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonBrowseFile;
+        private System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button Folder;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Error_Tolerance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Event handler ini tidak memiliki implementasi khusus.
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // Event handler ini tidak memiliki implementasi khusus.
+        }
+
+        private double persentaseErrorTolerance;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Tombol "Analyze" digunakan untuk menganalisis file yang telah dipilih.
+
+            string fileName = textBox1.Text;
+
+            try
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                int komputasiCount = 0;
+                int kontrolCount = 0;
+                int errorHandlerCount = 0;
+
+                foreach (string line in lines)
+                {
+                    // Memeriksa apakah baris mengandung komentar satu baris
+                    if (line.TrimStart().StartsWith("//"))
+                        continue;
+
+                    // Mengabaikan komentar dalam banyak bahasa pemrograman
+                    if (line.Contains("//"))
+                    {
+                        string codeLine = line.Split(new[] { "//" }, StringSplitOptions.None)[0].Trim();
+                        if (string.IsNullOrEmpty(codeLine))
+                            continue;
+                    }
+
+                    // Menghitung komputasi
+                    if (line.Contains("+") || line.Contains("-") || line.Contains("*") || line.Contains("/"))
+                    {
+                        komputasiCount++;
+                    }
+
+                    // Menghitung struktur kontrol
+                    if (line.Contains("if") || line.Contains("for") || line.Contains("while") || line.Contains("switch"))
+                    {
+                        kontrolCount++;
+                    }
+
+                    // Menghitung error handler
+                    if (line.Contains("catch"))
+                    {
+                        errorHandlerCount++;
+                    }
+                }
+
+                // Hitung persentase error tolerance
+                double persentaseErrorTolerance = (double)errorHandlerCount / (komputasiCount + kontrolCount) * 100;
+
+                // Menampilkan hasil analisis dalam textBox3
+                textBox3.Text = "Computations: " + komputasiCount + Environment.NewLine
+                            + "Control Structure: " + kontrolCount + Environment.NewLine
+                            + "Error Handler: " + errorHandlerCount + Environment.NewLine
+                            + "Error Tolerance: " + persentaseErrorTolerance.ToString("0.00") + "%";
+            }
+            catch (FileNotFoundException)
+            {
+                textBox3.Text = "File tidak ditemukan.";
+            }
+            catch (Exception ex)
+            {
+                textBox3.Text = "Terjadi kesalahan: " + ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Tombol "Browse" digunakan untuk memilih file yang akan dianalisis.
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Tidak ada filter file yang ditetapkan
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            // Event handler ini tidak memiliki implementasi khusus.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Tombol "Browse Folder" digunakan untuk memilih folder yang akan dianalisis.
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = folderBrowserDialog.SelectedPath;
+
+                    try
+                    {
+                        // Mendapatkan daftar file dalam folder
+                        string[] files = Directory.GetFiles(folderPath);
+
+                        // Analisis setiap file dalam folder
+                        int totalComputations = 0;
+                        int totalControlStructures = 0;
+                        int totalErrorHandlers = 0;
+
+                        foreach (string file in files)
+                        {
+                            try
+                            {
+                                string[] lines = File.ReadAllLines(file);
+                                int komputasiCount = 0;
+                                int kontrolCount = 0;
+                                int errorHandlerCount = 0;
+
+                                foreach (string line in lines)
+                                {
+                                    // Memeriksa apakah baris mengandung komentar satu baris
+                                    if (line.TrimStart().StartsWith("//"))
+                                        continue;
+
+                                    // Mengabaikan komentar dalam banyak bahasa pemrograman
+                                    if (line.Contains("//"))
+                                    {
+                                        string codeLine = line.Split(new[] { "//" }, StringSplitOptions.None)[0].Trim();
+                                        if (string.IsNullOrEmpty(codeLine))
+                                            continue;
+                                    }
+
+                                    // Menghitung komputasi
+                                    if (line.Contains("+") || line.Contains("-") || line.Contains("*") || line.Contains("/"))
+                                    {
+                                        komputasiCount++;
+                                    }
+
+                                    // Menghitung struktur kontrol
+                                    if (line.Contains("if") || line.Contains("for") || line.Contains("while") || line.Contains("switch"))
+                                    {
+                                        kontrolCount++;
+                                    }
+
+                                    // Menghitung error handler
+                                    if (line.Contains("catch"))
+                                    {
+                                        errorHandlerCount++;
+                                    }
+                                }
+
+                                // Menambahkan hasil analisis file ke total
+                                totalComputations += komputasiCount;
+                                totalControlStructures += kontrolCount;
+                                totalErrorHandlers += errorHandlerCount;
+                            }
+                            catch (Exception ex)
+                            {
+                                // Handle error ketika membaca file
+                                textBox3.Text += "Error saat membaca file '" + file + "': " + ex.Message + Environment.NewLine;
+                            }
+                        };
+
+                        // Hitung persentase error tolerance untuk keseluruhan folder
+                        double persentaseErrorTolerance = (double)totalErrorHandlers / (totalComputations + totalControlStructures) * 100;
+
+                        // Menampilkan hasil analisis keseluruhan folder dalam textBox3
+                        textBox3.Text = " Computations: " + totalComputations + Environment.NewLine
+                                    + " Control Structures: " + totalControlStructures + Environment.NewLine
+                                    + " Error Handlers: " + totalErrorHandlers + Environment.NewLine
+                                    + " Error Tolerance: " + persentaseErrorTolerance.ToString("0.00") + "%";
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle error ketika membaca folder
+                        textBox3.Text = "Error saat membaca folder: " + ex.Message;
+                    }
+                }
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            ShowHelpMessageBox();
+        }
+
+        private void ShowHelpMessageBox()
+        {
+            string helpMessage = GenerateHelpMessage();
+            MessageBox.Show(helpMessage, "User Guide", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private string GenerateHelpMessage()
+        {
+            StringBuilder helpMessage = new StringBuilder();
+
+            helpMessage.AppendLine("User Guide for Error Tolerance Application");
+            helpMessage.AppendLine();
+            helpMessage.AppendLine("1. Browse your file.");
+            helpMessage.AppendLine("2. Click 'Calculate' to run testing.");
+            helpMessage.AppendLine("3. The Apllication will show the result.");
+            helpMessage.AppendLine("4. Result will contain .");
+
+
+            return helpMessage.ToString();
+        }
+    }
+}
